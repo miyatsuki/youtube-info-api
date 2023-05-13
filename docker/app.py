1fcf497ef6b47e7ba43e5ebe0f33c4d829de5553
miyatsuki <6367574+miyatsuki@users.noreply.github.com>
2023-05-13 16:08:46 +0900

    fix: game_titleが複数ある場合は最初の要素だけ返す

--- docker/app.py
+++ docker/app.py
@@ -199,6 +199,13 @@ def extract_game_info(video_title: str, description: str):
     result = parse_llm_output_json(llm_result, model="gpt-4")
 
     ans = result.get("game_title")
+
+    if type(ans) == list:
+        if len(ans) > 0:
+            ans = ans[0]
+        else:
+            ans = None
+
     return ans
 
 
